ee907440c1b4792d3e59998658acfe2ce2b28755
andre <andre@10.146.192.79>
2025-04-07 11:25:54 -0300

    exercicio4

---
+++ src/main/java/com/mycompany/exercicio4/Exercicio4.java
@@ -0,0 +1,23 @@
+
+package com.mycompany.exercicio4;
+
+/**
+ *
+ * @author andre
+ */
+public class Exercicio4 {
+
+    public static void main(String[] args) {
+        String nome = "João";
+        System.out.println(nome);
+        nome = "Maria";
+        System.out.println("O valor da variável é:" + nome);
+    
+        
+        
+                
+              
+             
+        
+    }
+}
